64953d332ddc5ef123db3464b888b867ba34da38
samuelds <samuelds.pro@gmail.com>
2017-06-24 20:48:24 +0200

    Push Exemple

---
+++ __resource.lua
@@ -0,0 +1,18 @@
+-- @Author: samuelds
+-- @Date:   2017-06-08T23:28:39+02:00
+-- @Project: FiveM Tools
+-- @Last modified by:
+-- @Last modified time: 2017-06-15T21:30:46+02:00
+-- @License: GNU General Public License v3.0
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+dependency 'ft_areas'
+
+-- Scripts
+client_scripts {
+
+  "config.lua",
+  "cl_init.lua",
+
+}
---
+++ config.lua
@@ -0,0 +1,41 @@
+-- @Date:   2017-06-11T23:20:46+02:00
+-- @Project: FiveM Tools
+-- @Last modified time: 2017-06-24T20:38:33+02:00
+-- @License: GNU General Public License v3.0
+
+local inArea = false
+function callbackEnter()
+  Citizen.CreateThread(function()
+
+    SetNotificationTextEntry('STRING')
+    AddTextComponentString("Enter Area")
+    DrawNotification(false, false)
+
+  end)
+end
+
+function callbackExit()
+  Citizen.CreateThread(function()
+
+    SetNotificationTextEntry('STRING')
+    AddTextComponentString("Exit Area")
+    DrawNotification(false, false)
+
+  end)
+end
+
+function callbackActive()
+  Citizen.CreateThread(function()
+
+    SetNotificationTextEntry('STRING')
+    AddTextComponentString("Active Area")
+    DrawNotification(false, false)
+
+  end)
+end
+
+areas = {
+
+  ft_exemple = { x = -267.95840454102, y = -958.81805419922, z = 31.223133087158, weight = 2, enter = { callback = callbackEnter }, exit = { callback = callbackExit }, enable = true }
+
+}
